e971d57a42364fad736af9dca6c941ba652fe40e
Alex <alex.asesan@educa.jcyl.es>
2024-09-26 13:13:39 +0200

    Commit

---
+++ indexProyectoTema3.php
@@ -0,0 +1,46 @@
+<html>
+     <head>
+        <meta charset="UTF-8">               
+        <meta name="author" content="Alex Asensio Sanchez">
+        <meta name="application-name" content="indice">
+        <meta name="description" content="Indice tema 3">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+         <link rel="stylesheet" href="webroot/css/indexProyectoTema3.css">
+         <title>Alex Asensio Sanchez</title>
+     </head>
+     <body>
+         <header>
+             <h1>Proyecto tema 3</h1>
+         </header>
+        <!--highlight file(archivo)-->
+        <main>             
+            <table>
+                <thead>
+                    <th>nº</th>
+                    <th>Enunciado</th>
+                    <th>Ejecutar</th>
+                    <th>Mostrar</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>0</td>
+                        <td>Hola mundo y phpinfo()</td>
+                        <td><a><image src="webroot/images/play.png" heigth="10%" width="10%"/></a></td>
+                        <td><a><image src="webroot/images/ojo.png" heigth="3%" width="3%"/></a></td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Inicializar variables de los distintos tipos de datos básicos(string, int, float, bool) y mostrar los datos por pantalla (echo, print, printf, print_r,var_dump).</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </main>
+        <footer>
+            <p><a href="../index.html">Alex Asensio Sanchez</a></p>
+            <p><a href="../204DWESProyectoDWES/indexProyectoDWES.php">DWES</a></p>
+            <p>Repositorio del proyecto</p>
+        </footer>
+     </body>
+ </html>
