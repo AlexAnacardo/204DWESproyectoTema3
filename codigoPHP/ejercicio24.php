ebe4d81b79552d9be5d68938ed610ccc063ff56e
Alex <alex.asesan@educa.jcyl.es>
2024-10-22 13:55:02 +0200

    Commit 22/10/2024 final dia

--- codigoPHP/ejercicio24.php
+++ codigoPHP/ejercicio24.php
@@ -16,13 +16,17 @@
                  
                 //importamos la libreria de vaidaciones
                 require_once('../core/231018libreriaValidacion.php');
-                 
+                $entradaOK=true; //Booleano que confirma que todo va bien
+                $oFechaActual = new DateTime("now"); //Variable que recoge la fecha actual
+                
                 $aErrores=[  //Array de errores
-                    'nombre'=>" ",
-                    'edad'=>" ",
-                    'fechaNac'=>" ",
-                    'email'=>" ",
-                    'real'=>" ",
+                    'nombre' => '',
+                    'edad'=>'',
+                    'fechaNac'=>'',
+                    'email'=>'',
+                    'real'=>'',
+                    'telefono'=>'',
+                    'url'=>'',                    
                 ]; 
                 $aRespuestas=[  //Array de respuestas
                     'nombre'=>" ",
@@ -30,9 +34,11 @@
                     'fechaNac'=>" ",
                     'email'=>" ",
                     'real'=>0.0,
+                    'telefono'=>" ",
+                    'url'=>" ",
+                    'fechaActual'=>$oFechaActual,
                 ]; 
-                $entradaOK=true; //Booleano que confirma que todo va bien
-                
+                 
                 //Cuando se envien los datos, se entrara aqui para validarlos
                 if(isset($_REQUEST['enviar'])){
                         
@@ -42,12 +48,16 @@
                             'fechaNac'=> validacionFormularios::validarFecha($_REQUEST['fechaNac'], '01/01/2200'),
                             'email'=> validacionFormularios::validarEmail($_REQUEST['email']),
                             'real'=> validacionFormularios::comprobarFloat($_REQUEST['real']),
+                            'telefono'=> validacionFormularios::validarTelefono($_REQUEST['telefono']),
+                            'url'=> validacionFormularios::validarURL($_REQUEST['url']),                            
                         ];   
                     
-                    //Se recorre el array, si hay errores el booleano entradaOK sera false y el formulario no se enviara
+                     //Recorremos el array de errores 
                     foreach ($aErrores as $clave => $valor) {
                         if ($valor == !null) {
                             $entradaOK = false;
+                            //Limpiamos el campo si hay un error
+                            $_REQUEST[$clave] = ''; 
                         }
                     }
                 }
@@ -58,16 +68,19 @@
                 if($entradaOK){
                     $aRespuestas['nombre']=$_REQUEST['nombre'];
                     $aRespuestas['edad']=$_REQUEST['edad'];
-                    $aRespuestas['fechaNac']=$_REQUEST['fechaNac'];
+                    $aRespuestas['fechaNac']=new DateTime($_REQUEST['fechaNac']);
                     $aRespuestas['email']=$_REQUEST['email'];
                     $aRespuestas['real']=$_REQUEST['real'];
+                    $aRespuestas['telefono']=$_REQUEST['telefono'];
+                    $aRespuestas['url']=$_REQUEST['url'];                    
                     
                 ?>
                     <div id="respuesta">                        
                 <?php
                         echo "<h1>Respuestas:</h1>";
                         foreach ($aRespuestas as $key => $value) {
-                            echo "$key : $value <br>";
+                            //echo "$key : $value <br>";
+                            echo ($value instanceof DateTime) ? "$key : " . $value->format('d/m/Y') . "<br>" : "$key : $value <br>";  
                         }
                 ?>
                     </div>
@@ -79,7 +92,7 @@
                 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" novalidate>
                         <div id="divNombre">
                             <label for="nombre">Nombre:</label><br>
-                            <input type="text" id="nombre" name="nombre" style="background-color: yellow" value="<?php echo (!$aErrores['nombre'] == "" ? '' : $_REQUEST['nombre']); ?>"/><!-- Si el array de errores esta vacio y la variable "nombre" tiene un valor ya validado, este aparecera en el campo de haber un error en otro de los campos -->
+                            <input type="text" id="nombre" name="nombre" placeholder="Tu nombre" style="background-color: yellow" value="<?php echo (isset($_REQUEST['nombre']) ? $_REQUEST['nombre'] : ''); ?>"/><!-- Si el array de errores esta vacio y la variable "nombre" tiene un valor ya validado, este aparecera en el campo de haber un error en otro de los campos -->
                             <?php if (!empty($aErrores["nombre"])) { ?>
                                     <!--Si hay algun error almacenado en el array, el mensaje del mismo se mostrara, esto para cada caso-->
                                     <p style="color: red"><?php echo $aErrores["nombre"]; ?></p>
@@ -87,15 +100,15 @@
                         </div>
                         <div id="divEdad">
                             <label for="edad">Edad:</label><br>
-                            <input type="text" id="edad" name="edad" value="<?php echo (!$aErrores['edad'] == "" ? '' : $_REQUEST['edad']); ?>"/><!-- Si el array de errores esta vacio y la variable "nombre" tiene un valor ya validado, este aparecera en el campo de haber un error en otro de los campos -->
+                            <input type="text" id="edad" name="edad" value="<?php echo (isset($_REQUEST['edad']) ? $_REQUEST['edad'] : ''); ?>"/><!-- Si el array de errores esta vacio y la variable "nombre" tiene un valor ya validado, este aparecera en el campo de haber un error en otro de los campos -->
 
                             <?php if (!empty($aErrores["edad"])) { ?>
-                                    <p id="errorFecha" style="color:red"><?php echo $aErrores["edad"]; ?></p>
+                                    <p id="errorEdad" style="color:red"><?php echo $aErrores["edad"]; ?></p>
                             <?php } ?>
                         </div>
                         <div id="divFechaNacimiento">
                             <label for="fechaNac">Fecha de nacimiento:</label><br>
-                            <input type="date" id="fechaNac" name="fechaNac" value="<?php echo (!$aErrores['fechaNac'] == "" ? '' : $_REQUEST['fechaNac']); ?>"/><!-- Si el array de errores esta vacio y la variable "nombre" tiene un valor ya validado, este aparecera en el campo de haber un error en otro de los campos -->
+                            <input type="date" id="fechaNac" name="fechaNac" value="<?php echo (isset($_REQUEST['fechaNac']) ? $_REQUEST['fechaNac'] : ''); ?>"/><!-- Si el array de errores esta vacio y la variable "nombre" tiene un valor ya validado, este aparecera en el campo de haber un error en otro de los campos -->
 
                             <?php if (!empty($aErrores['fechaNac'])) { ?>
                                     <p id="errorFecha" style="color:red"><?php echo $aErrores['fechaNac']; ?></p>
@@ -103,7 +116,7 @@
                         </div>
                         <div id="divEmail">
                             <label for="email">Email personal:</label><br>
-                            <input type="mail" id="email" name="email" value="<?php echo (!$aErrores['email'] == "" ? '' : $_REQUEST['email']); ?>"/><!-- Si el array de errores esta vacio y la variable "nombre" tiene un valor ya validado, este aparecera en el campo de haber un error en otro de los campos -->
+                            <input type="mail" id="email" name="email" placeholder="xxxxxx@xxxx.com" value="<?php echo (isset($_REQUEST['email']) ? $_REQUEST['email'] : ''); ?>"/><!-- Si el array de errores esta vacio y la variable "nombre" tiene un valor ya validado, este aparecera en el campo de haber un error en otro de los campos -->
 
                             <?php if (!empty($aErrores['email'])) { ?>
                                     <p id="errorEmail" style="color:red"><?php echo $aErrores['email']; ?></p>
@@ -111,11 +124,31 @@
                         </div>
                         <div id="divReal">
                             <label for="real">Mete un float:</label><br>
-                            <input type="text" id="real" name="real" value="<?php echo (!$aErrores['real'] == "" ? '' : $_REQUEST['real']); ?>"/><!-- Si el array de errores esta vacio y la variable "nombre" tiene un valor ya validado, este aparecera en el campo de haber un error en otro de los campos -->
+                            <input type="text" id="real" name="real" value="<?php echo (isset($_REQUEST['real']) ? $_REQUEST['real'] : ''); ?>"/><!-- Si el array de errores esta vacio y la variable "nombre" tiene un valor ya validado, este aparecera en el campo de haber un error en otro de los campos -->
 
                             <?php if (!empty($aErrores['real'])) { ?>
                                     <p id="errorReal" style="color:red"><?php echo $aErrores['real']; ?></p>
                             <?php } ?>
+                        </div>
+                        <div id="divTelefono">
+                            <label for="telefono">Telefono:</label><br>
+                            <input type="tel" id="telefono" name="telefono" placeholder="000000000" value="<?php echo (isset($_REQUEST['telefono']) ? $_REQUEST['telefono'] : ''); ?>"/><!-- Si el array de errores esta vacio y la variable "nombre" tiene un valor ya validado, este aparecera en el campo de haber un error en otro de los campos -->
+
+                            <?php if (!empty($aErrores['telefono'])) { ?>
+                                    <p id="errorTelefono" style="color:red"><?php echo $aErrores['telefono']; ?></p>
+                            <?php } ?>
+                        </div>
+                        <div id="divUrl">
+                            <label for="url">Introduce una url:</label><br>
+                            <input type="url" id="url" name="url" placeholder="http://..." value="<?php echo (isset($_REQUEST['url']) ? $_REQUEST['url'] : ''); ?>"/><!-- Si el array de errores esta vacio y la variable "nombre" tiene un valor ya validado, este aparecera en el campo de haber un error en otro de los campos -->
+
+                            <?php if (!empty($aErrores['url'])) { ?>
+                                    <p id="errorUrl" style="color:red"><?php echo $aErrores['url']; ?></p>
+                            <?php } ?>
+                        </div>
+                        <div id="divFechaActual">
+                            <label for="fechaActual">Fecha actual:</label><br>
+                            <input type="text" id="fechaActual" name="fechaActual"  style="background-color: lightgray"  value="<?php echo date_format($oFechaActual, "d-m-Y")?>" disabled>
                         </div>
                         <div id="enviar">
                             <input id="boton" name="enviar" type="submit" value="Enviar">
