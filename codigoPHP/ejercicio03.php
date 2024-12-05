62a3d622c8a45975d0c3bdc3967c2fae6b2a0341
Alex <alex.asesan@educa.jcyl.es>
2024-12-05 13:12:00 +0100

    Pruebas ej 3

--- codigoPHP/ejercicio03.php
+++ codigoPHP/ejercicio03.php
@@ -13,37 +13,28 @@
              * @version 2024/10/1
              * @author Alex Asensio Sanchez                          
              */
-                
-            
-            /*Hola, hoy es 2 de octubre de 2024 y son las xx:xx horas en benavente, las xx:xx en Oporto, naci el DD de MES de AÑO y por tanto tengo
-            XX años que es lo mismo que xx dias. El 1 de enero de 2050 tendre XX años*/                
-                
-            /*
-                $oFechaActual=new DateTime('now', new DateTimeZone("Europe/Madrid")); 
-                $oFechaNacimiento=new DateTime("2004-07-19", new DateTimeZone("Europe/Madrid"));
-                echo("Hola, hoy es ".$oFechaActual->format("j")." de ".$oFechaActual->format("F")." de ".$oFechaActual->format("Y"));                
-                echo(" y son las ".$oFechaActual->format("H").":".$oFechaActual->format("i")." horas en benavente, ");
-                $oFechaActual->setTimezone(new DateTimeZone("Europe/Lisbon"));
-                echo("las ".$oFechaActual->format("H").":".$oFechaActual->format("i")." en Oporto. <br>");
-                $oFechaActual->setTimezone(new DateTimeZone("Europe/Madrid"));
-                echo("Naci el ".$oFechaNacimiento->format("j")." de ".$oFechaNacimiento->format("F")." de ".$oFechaNacimiento->format("Y")." y por tanto tengo ".($oFechaActual->diff($oFechaNacimiento))->format("%y")." años,");
-                echo("que es lo mismo que ".(date_diff($oFechaNacimiento, $oFechaActual))->format("%a")." dias. <br>");
-                echo("El 1 de enero de 2050 tendre ".($oFechaNacimiento->diff(new DateTime("2050-07-19")))->format("%y")." años");                           
-             */
+                          
             $ok = setLocale(LC_ALL,"es_ES");
             date_default_timezone_set("Europe/Madrid");
             
-            $oFechaActual=new DateTime('now', new DateTimeZone("Europe/Madrid")); 
-                $oFechaNacimiento=new DateTime("2004-07-19", new DateTimeZone("Europe/Madrid"));
-                echo("Hola, hoy es ".$oFechaActual->format("j")." de ".$oFechaActual->format("F")." de ".$oFechaActual->format("Y"));                
-                echo(" y son las ".$oFechaActual->format("H").":".$oFechaActual->format("i")." horas en benavente, ");
-                $oFechaActual->setTimezone(new DateTimeZone("Europe/Lisbon"));
-                echo("las ".$oFechaActual->format("H").":".$oFechaActual->format("i")." en Oporto. <br>");
-                $oFechaActual->setTimezone(new DateTimeZone("Europe/Madrid"));
-                echo("Naci el ".$oFechaNacimiento->format("j")." de ".$oFechaNacimiento->format("F")." de ".$oFechaNacimiento->format("Y")." y por tanto tengo ".($oFechaActual->diff($oFechaNacimiento))->format("%y")." años,");
-                echo("que es lo mismo que ".(date_diff($oFechaNacimiento, $oFechaActual))->format("%a")." dias. <br>");
-                echo("El 1 de enero de 2050 tendre ".($oFechaNacimiento->diff(new DateTime("2050-07-19")))->format("%y")." años");
-            ?>
+            $oFechaActual = new DateTime('now', new DateTimeZone("Europe/Madrid"));
+            
+            $oFechaNacimiento = new DateTime("2004-07-19", new DateTimeZone("Europe/Madrid"));
+            
+            echo("Hola, hoy es " . $oFechaActual->format("j") . " de " . $oFechaActual->format("F") . " de " . $oFechaActual->format("Y"));
+            echo(" y son las " . $oFechaActual->format("H") . ":" . $oFechaActual->format("i") . " horas en benavente, ");
+            
+            $oFechaActual->setTimezone(new DateTimeZone("Europe/Lisbon"));
+            
+            echo("las " . $oFechaActual->format("H") . ":" . $oFechaActual->format("i") . " en Oporto. <br>");
+            
+            $oFechaActual->setTimezone(new DateTimeZone("Europe/Madrid"));
+            
+            echo("Naci el " . $oFechaNacimiento->format("j") . " de " . $oFechaNacimiento->format("F") . " de " . $oFechaNacimiento->format("Y") . " y por tanto tengo " . ($oFechaActual->diff($oFechaNacimiento))->format("%y") . " años,");
+            echo("que es lo mismo que " . (date_diff($oFechaNacimiento, $oFechaActual))->format("%a") . " dias. <br>");
+            echo("El 1 de enero de 2050 tendre " . ($oFechaNacimiento->diff(new DateTime("2050-07-19")))->format("%y") . " años");
+           
+?>
         </p>
         <footer>
             <p><a href="../../index.html">Alex Asensio Sanchez</a></p>
